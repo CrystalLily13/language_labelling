9c83353eafae60da5c43be04c5dd6efe5afad4f0
Crystal <crystallily1302@gmail.com>
2025-01-23 15:50:51 -0500

    added loop to iterate over songs with playback

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+# audio folder from google drive placed at root of project
+# didn't include that in the project files for obvious reasons
 
 
+from os import walk, path
+import vlc
+import songs
+
+def enumerate_files(audio_path: str):
+    for (dirpath, _dirnames, filenames) in walk(audio_path):
+        for name in filenames:
+            yield path.join(dirpath, name)
 
 def main():
-    pass
+    audio_path = path.join(path.abspath(path.curdir), "audio")
+    song_data = songs.SongData()
+    for name in enumerate_files(audio_path):
+        print(name)
+        player = vlc.MediaPlayer("file://"+path.join(audio_path, name))
+        player.play()
+        input("Press Enter to go next")
+        player.stop()
 
 
 if __name__ == "__main__":
---
+++ songs.py
@@ -0,0 +1,14 @@
+
+class SongData:
+
+    # map of song paths to the language call and a boolean for if the song has multiple languages in it
+    data: dict[str, (str, bool)] = {}
+
+    def get(self, key) -> tuple[str, bool]:
+        return self.data.get(key)
+    
+    def set(self, key, value: tuple[str, bool]):
+        self.data[key] = value
+
+    def contains(self, key) -> bool:
+        return key in self.data.keys()
